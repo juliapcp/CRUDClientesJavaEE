66f26455ebc45be8efb7e97d86aad79313fc4925
JuliaPontes <juliapcp2003@gmail.com>
2023-09-20 22:50:19 -0300

    Desenvolvimento do DAO

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,22 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	private String driver = "org.postgresql.Driver";
+	private String url = "jdbc:postgresql://127.0.0.1:5432/projeto_gerenciamento?useTimezone=true&serverTimezone=UTC";
+	private String user = "postgres";
+	private String password = "postgres";
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user ,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -1,5 +1,9 @@
 package model;
 
 public class JavaBeans {
-
+	private String uuid;
+	private String nome;
+	private String email;
+	private String telefone;
+	
 }
